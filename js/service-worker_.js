9740bbfcffc94a5ec6c6d18839d87b5c66e979b0
Juan Carlos Arias Ambriz <juancarlos.arias@gmail.com>
2017-04-10 14:25:20 -0500

    service worker

--- js/service-worker_.js
+++ js/service-worker_.js
@@ -5,18 +5,4 @@ self.addEventListener('install', function(event){
 self.addEventListener('activate', function(event){
     console.log('Activate', event);
 });
-
-self.addEventListener('fetch', function(event){
-    var url = new URL(event.request.url);
-    console.log('Fetch', event, url.pathname);
-    event.respondWith(
-        fetch(event.request).then(function(response){
-            if (response.status == 404)
-            {
-                return fetch('/images/logo.png')
-            }
-            return response;
-        })
-    );
-    
-})
+console.log("service worker")
