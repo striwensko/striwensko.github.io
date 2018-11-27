960fd231b12089793e9b17dd0da857f24661a5a2
Juan Carlos <juan.arias@mobovida.com>
2018-11-27 16:51:53 -0600

    gamepix

--- scripts/gamePix.js
+++ scripts/gamePix.js
@@ -193,7 +193,7 @@
             display: inline-block;
             margin: 15px;
         }
-        @media only screen and (min-width:801px)
+        @media only screen and (max-width:600px)
         {
             .catalog .catalog-list .game-item{
                 width: 130px;
