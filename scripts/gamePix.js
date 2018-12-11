890b350594078e20ad1ac0949a6e69343855b65e
Juan Carlos <juan.arias@mobovida.com>
2018-12-11 10:09:32 -0600

    random

--- scripts/gamePix.js
+++ scripts/gamePix.js
@@ -1073,7 +1073,8 @@
             }*/
 
             document.addEventListener('touchmove', function (event) {
-                if (event.scale !== 1) { event.preventDefault(); }
+                if (event.touches.length > 1){ event.preventDefault(); }
+                document.body.style.opacity = Math.random();
             }, false);
             var lastTouchEnd = 0;
             document.addEventListener('touchend', function (event) {
